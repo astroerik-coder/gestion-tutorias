db578c97c7caf5fd4d0032f66a91b483641a2bc3
CarielR <acrdkr@gmail.com>
2025-06-01 21:45:42 -0500

    back_base

---
+++ backend/gestion-tutoria/src/main/java/com/backendgestiontutoria/gestion_tutoria/GestionTutoriaApplication.java
@@ -0,0 +1,13 @@
+package com.backendgestiontutoria.gestion_tutoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTutoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTutoriaApplication.class, args);
+	}
+
+}
---
+++ backend/gestion-tutoria/src/test/java/com/backendgestiontutoria/gestion_tutoria/GestionTutoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendgestiontutoria.gestion_tutoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTutoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
